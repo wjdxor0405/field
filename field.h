73861a01a405311d2528e2fe2c0ff562493feab4
wjdxor0405 <wjdxor0405@gmail.com>
2022-05-04 21:24:59 +0900

    constuctor destructor to public

--- field.h
+++ field.h
@@ -34,15 +34,16 @@ class field_num{
 
 class Number_field : Field{
     private:
-        Number_field();
-        ~Number_field();
+        // Number_field();
+        // ~Number_field();
     public:
         field_num f_num;
         // field_type type;
         Poly poly;
         // Number_field(field_num input_num){
-        Number_field(field_num field_num);
+        Number_field(field_num field_num);//need to implement
         Number_field(Poly min_poly);
+        Number_field();
         ~Number_field();
         void print_info();
 };
